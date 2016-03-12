f1cc07b5785e73d5460b2d9422ec7d296c08c8c5
guybedford <guybedford@gmail.com>
2016-03-12 16:01:10 +0200

    better guards

--- node-binary.js
+++ node-binary.js
@@ -2,8 +2,29 @@ var isWindows = typeof process != 'undefined' && process.platform && process.pla
 
 exports.build = false;
 
-exports.instantiate = function(load) {
-  if (load.name.substr(load.name.length - 5, 5) != '.node')
+var fs;
+exports.fetch = function(load) {
+  // if in the browser, throw a compatibility error
+  if (typeof process == 'undefined' || typeof process.versions == 'undefined' || !process.versions.node)
+    throw new Error('Node binary files can only be loaded in Node.');
+
+  if (load.name.substr(load.address.length - 5, 5) != '.node')
     throw new Error('Node binaries must end in ".node"');
-  return this._nodeRequire(load.name.substr(isWindows ? 8 : 7));
+
+  // we can do this because this code is 100% only ever in Node
+  fs = fs || this._nodeRequire('fs');
+
+  // fetch just needs to verify the file exists
+  return new Promise(function(resolve, reject) {
+    fs.exists(load.address, function(exists) {
+      if (!exists)
+        reject(new Error('Node binary file ' + load.address + ' does not exist.'));
+      else
+        resolve();
+    });
+  });
+};
+
+exports.instantiate = function(load) {
+  return this._nodeRequire(load.address.substr(isWindows ? 8 : 7));
 };
